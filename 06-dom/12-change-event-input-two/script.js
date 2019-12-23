ae083b8828a2e06066ccef58122c4f256ce80ea1
dimitry <dimiw1337@hotmail.com>
2019-12-23 05:47:11 -0500

    06-dom/12-change-event-input-two

--- 06-dom/12-change-event-input-two/script.js
+++ 06-dom/12-change-event-input-two/script.js
@@ -11,6 +11,29 @@
 
 (function() {
 
-    // your code here
+    let validity = document.getElementById("validity");
+    let write = document.getElementById("pass-one");
+
+    write.addEventListener("input", function () {
+        let number = 0;
+        let num = write.value;
+        let inputTotal = num.length;
+        //loop
+        for (i = 0; i < inputTotal; i++) {
+        let char = num.charAt(i);
+            //loop
+
+            for (b = 0; b < 9; b++) {
+
+                //if statement
+                if (b === parseInt(char)) {
+                    number++;
+                }}}
+
+        //if statement
+        if ((inputTotal >= 8) && (number >= 2)) {
+            validity.innerHTML = "Ok";
+        }
+    });
 
 })();
