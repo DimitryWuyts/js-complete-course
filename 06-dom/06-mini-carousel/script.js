80bd1bf1621104832ece727014f83e233a3d850e
dimitry <dimiw1337@hotmail.com>
2019-12-23 03:53:33 -0500

    06-dom/06-mini-carousel

--- 06-dom/06-mini-carousel/script.js
+++ 06-dom/06-mini-carousel/script.js
@@ -11,14 +11,31 @@
 
 (function() {
 
-    var gallery= [
-        "../../_shared/img/bell.svg",
-        "../../_shared/img/clock.svg",
+    //var images
+
+    let gallery= [
+        "../../_shared/img/map.svg",
         "../../_shared/img/compass.svg",
+        "../../_shared/img/clock.svg",
         "../../_shared/img/lemon.svg",
         "../../_shared/img/map.svg",
     ];
 
-    // your code here
+    let image = document.querySelector("img");
+    let i = 0;
+
+    document.getElementById("next").addEventListener("click", function () {
+
+        //if statement
+
+        if (i < 4){
+            i++;
+            image.setAttribute("src",  gallery[i]);
+        } else {
+            i = 0;
+            image.setAttribute("src", gallery[i]);
+        }
+
+    })
 
 })();
