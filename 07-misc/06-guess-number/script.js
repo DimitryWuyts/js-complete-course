9855700baea2cab2408f29ad2a32dbdb2e5ee0a3
dimitry <dimiw1337@hotmail.com>
2020-01-02 04:43:40 -0500

    07-misc/06-guess-number

--- 07-misc/06-guess-number/script.js
+++ 07-misc/06-guess-number/script.js
@@ -11,6 +11,27 @@
 
 (function() {
 
-    // your code here
+    var attempts = 0;
+    var myNumbers = Math.floor(Math.random() * 100);
+    var inputNumbers = prompt("Enter a number between 1 and 100");
 
+    while (true) {
+        attempts++;
+        if (inputNumbers < myNumbers)
+            inputNumbers = prompt("Higher");
+
+        else if (inputNumbers > myNumbers)
+            inputNumbers = prompt("Lower");
+
+        else {
+            alert(
+                "Well done! You needed " +
+                attempts +
+                " Attempts to find the number"
+            );
+
+            break;
+        }
+    }
 })();
+
