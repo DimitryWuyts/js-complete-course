013c8d6a4cbfa3c48222788275fb964f0c103ddb
dimitry <dimiw1337@hotmail.com>
2020-01-02 03:49:51 -0500

    07-misc/04-worst-ui-two

--- 07-misc/04-worst-ui-two/script.js
+++ 07-misc/04-worst-ui-two/script.js
@@ -11,6 +11,36 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("target").innerText = "460000000";
+    var button = document.querySelectorAll("button");
 
+        button.forEach((button) => {
+        button.addEventListener('click', () => {
+            var max = button.getAttribute("data-max");
+            var min = button.getAttribute("data-min");
+            var local = parseInt(button.innerText,10);
+            if (local < max){
+                local++;
+                button.innerHTML = local;
+            }
+            else{
+                button.innerHTML = min;
+            }
+
+            if (local < 10){
+                button.innerHTML = "0" + local;
+            }
+            var one = document.getElementById("part-one")
+                .innerHTML;
+            var two = document.getElementById("part-two")
+                .innerHTML;
+            var three = document.getElementById("part-three")
+                .innerHTML;
+            var four = document.getElementById("part-four")
+                .innerHTML;
+            console.log(one);
+            document.getElementById("target").innerHTML = one +
+                two + three + four ;
+        });
+    });
 })();
