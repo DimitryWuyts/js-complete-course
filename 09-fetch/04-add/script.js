71e389a8e1b532eb3bb2eea3a394db0aff6ca70f
dimitry <dimiw1337@hotmail.com>
2020-01-06 06:29:28 -0500

    09-fetch/04-add

--- 09-fetch/04-add/script.js
+++ 09-fetch/04-add/script.js
@@ -10,5 +10,29 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    fetch("http://localhost:63342/js-complete-course/_shared/api.json")
+        .then(function(response){
+            return response.json();
+        })
+
+        .then(function(data){
+
+            var array = data.heroes;
+
+            document.getElementById("run").addEventListener("click", function(){
+
+
+                var num = array.length + 1;
+                var name = document.getElementById("hero-name").value;
+                var alter = document.getElementById("hero-alter-ego").value;
+                var power = document.getElementById("hero-powers").value;
+                power = power.split(", ");
+
+                array.push({"id": num, "name": name, "alterEgo": alter, "abilities": power});
+
+                console.log(array);
+
+            })
+        })
 })();
