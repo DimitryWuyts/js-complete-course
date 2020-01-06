109eccbfa9e0c7cd64a41bee71921c656cdcca76
dimitry <dimiw1337@hotmail.com>
2020-01-06 04:30:53 -0500

    09-fetch/01--list-to-console

--- 09-fetch/01-list-to-console/script.js
+++ 09-fetch/01-list-to-console/script.js
@@ -10,5 +10,14 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+
+        fetch("http://localhost:63342/js-complete-course/_shared/api.json")
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data);
+            })
+    })
 })();
